70ee4093f294bf60893d516c0ae875fbe22592fe
alinafere <ferecatu@rsm.nl>
2020-09-03 09:12:27 +0200

    added example 1

---
+++ example1.R
@@ -0,0 +1,37 @@
+# --------------------------------
+# Example 1 R Script
+# --------------------------------
+
+# Create a vector containing the ages of 5 students
+c(22.4, 21.5, 23.7, 21.8, 20.8)
+
+# Assign this vector to the name 'age' so that we can reuse it
+age <- c(22.4, 21.5, 23.7, 21.8, 28.8)
+
+# Type the object name to print its content
+age
+
+# R is case-sensitive
+Age
+AGE
+
+# Read the help file for the mean() function
+?mean
+
+# Calculate the mean age in years
+mean(age)
+
+# Remember to close the parentheses
+mean(age
+)
+
+# Store the mean age in an object named 'mean_age'
+mean_age <- mean(age)
+
+# Print the mean of 'age'
+mean_age
+
+
+# mean_age = mean(age)
+
+
